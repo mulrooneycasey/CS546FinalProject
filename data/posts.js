f1ad1e65b5585adf180270aef54c11d1b402b8b9
Nicholas Mule <nickmule77@gmail.com>
2022-12-14 14:17:29 -0500

    helpers
    Just added a helpers file to assist with any error checking

--- data/posts.js
+++ data/posts.js
@@ -2,5 +2,6 @@ const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
 const { ObjectId } = require('mongodb');
 const bcrypt = require('bcrypt');
+const helpers = require('../helpers');
 
 module.exports = {};

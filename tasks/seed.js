08cb65ca6513282b5eb55a9c55638b191321af62
Andrew Capro <acapro@stevens.edu>
2022-12-14 14:22:16 -0500

    added logo and nicks picture

--- tasks/seed.js
+++ tasks/seed.js
@@ -19,7 +19,7 @@ async function main() {
     await users.createUser('Nick', 'Mule', 'nmule@gmail.com', 'nmule', 'nick123', 'Hoboken', 'New Jersey')
     const nPost = await posts.createPost('Nick', 'Mule', 'an object 3', 'image3.png', '800 Washington St.')
     const nPostID = nPost._id.toString()
-    await posts.createReview(aPostID, 'nmule', 'This is a cool thing!')
+    await posts.createReview(aPostID, 'nmule', 'This is a cool thing!', 5)
     await posts.createComment(aPostID, 'nmule', 'Its actually hidden under the stairs')
 
     //user three
@@ -27,13 +27,13 @@ async function main() {
     const cPost1 = await posts.createPost('Casey', 'Mulrooney', 'an object 4', 'image4.png', '300 Bloomfield Ave.')
     await posts.createPost('Casey', 'Mulrooney', 'an object 5', 'image5.png', 'Outside Babbio Center')
     const cPostID = cPost1._id.toString()
-    await posts.createReview(nPostID, 'cmulrooney', 'This is fake!')
+    await posts.createReview(nPostID, 'cmulrooney', 'This is fake!', 4)
     await posts.createComment(aPostID, 'cmulrooney', 'nmule is wrong, someone already took it!')
 
     //user four
     await users.createUser('Chancelor', 'Assiamah', 'cassiamah@gmail.com', 'cassiamah', 'chancelor123', 'Hoboken', 'New Jersey')
     const chPost = await posts.createPost('Chancelor', 'Assiamah', 'an object 6', 'image6.png', '800 Washington St.')
-    await posts.createReview(cPostID, 'cassiamah', 'It was exactly where the picture depicted!')
+    await posts.createReview(cPostID, 'cassiamah', 'It was exactly where the picture depicted!', 3)
     await posts.createComment(nPostID, 'cassiamah', 'no where to be found')
 
     console.log('Done seeding')
